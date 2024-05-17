002dcfa4e763356efb6107c607c2d59c8796d467
Vansh353 <vansh.malhotra@seaflux.tech>
2024-05-17 10:32:58 +0530

    MLflow code added

--- src/components/model_evaluation.py
+++ src/components/model_evaluation.py
@@ -1,35 +1,60 @@
 import os
 import sys
-from sklearn.metrics import r2_score, mean_absolute_error,mean_squared_error
-from urllib.parse import urlparse
 import mlflow
-import pandas as pd
 import mlflow.sklearn
 import numpy as np
 import pickle
-from src.exception.exception import customexception
 from src.utils.utils import load_object
+from urllib.parse import urlparse
+from sklearn.metrics import mean_squared_error,mean_absolute_error,r2_score
 from src.logger.logging import logging
-from dataclasses import dataclass
-@dataclass
-class ModelEvaluationConfig:
-    """
-    This class will be used to store all the configuration for data injestion
-    """
-class ModelEvaluationC:
+from src.exception. exception import customexception
+
+class ModelEvaluation:
     def __init__(self):
-        pass
-    
-    def initiate_model_evaluation(self):
-        """
-        This function will be used to initiate the data injestion process
-        """
+        logging.info("evaluation started")
+
+    def eval_metrics(self,actual,pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))# here is RMSE
+        mae = mean_absolute_error(actual, pred)# here is MAE
+        r2 = r2_score(actual, pred)# here is r3 value
+        logging.info("evaluation metrics captured")
+        return rmse, mae, r2
+
+    def initiate_model_evaluation(self,train_array,test_array):
         try:
-            logging.info("Initiating data injestion process")
-            # Read the data
-            data = pd.read_csv("https://raw.githubusercontent.com/dphi-official/Datasets/master/heart_disease.csv")
-            logging.info("Data injested successfully")
-            return data
+             X_test,y_test=(test_array[:,:-1], test_array[:,-1])
+
+             model_path=os.path.join("artifacts","model.pkl")
+             model=load_object(model_path)
+
+             #mlflow.set_registry_uri("")
+             
+             logging.info("model has register")
+
+             tracking_url_type_store=urlparse(mlflow.get_tracking_uri()).scheme
+
+             print(tracking_url_type_store)
+
+
+
+             with mlflow.start_run():
+
+                prediction=model.predict(X_test)
+
+                (rmse,mae,r2)=self.eval_metrics(y_test,prediction)
+
+                mlflow.log_metric("rmse", rmse)
+                mlflow.log_metric("r2", r2)
+                mlflow.log_metric("mae", mae)
+
+                 # Model registry does not work with file store
+                if tracking_url_type_store != "file":
+
+                    mlflow.sklearn.log_model(model, "model", registered_model_name="DiamondPredictionModel")
+                else:
+                    mlflow.sklearn.log_model(model, "model")
+
+
         except Exception as e:
-                logging.info()
-                raise customexception(e,sys)
+            raise customexception(e,sys)
